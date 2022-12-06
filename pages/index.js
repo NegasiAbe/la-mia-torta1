f0f6f183343aa9a30e3a11ded9451e15bec34489
yosefg-dev <jossmicheal@gmail.com>
2022-12-06 11:18:14 +0100

    working the structure

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+//import {getSession, signIn, signOut} from 'next-auth/react'; 
 
 export default function Home(props) {
-
+  const curUser = props.currentUser;
   return (
     <>
       <h1>La Mia Torta Home Page</h1>
     </>
   )
 }
+
+
