be5ec86712429c3ee3707943887e7a8e37336fe6
yeshey choden <yeshey@DESKTOP-ES2KOVM.localdomain>
2022-12-07 17:14:03 +0100

    changed sign in name

--- components/navbar1.jsx
+++ components/navbar1.jsx
@@ -13,7 +13,7 @@ export default function Component({ curuser }) {
         <nav className={styles.navbar}>
             <div className={styles.logo}>
                 <Image src='https://tse1.mm.bing.net/th?id=OIP.qOt_5rWJneT6ZgdY57duVwHaFj&pid=Api&rs=1&c=1&qlt=95&w=143&h=107'
-                 alt="Picture of the logo" width={50} height={30} 
+                 alt="Picture of the logo" width={50} height={50} 
                 className={styles.navlogo}/>
             </div>
             <div className={styles.navlink}>
@@ -25,7 +25,7 @@ export default function Component({ curuser }) {
             <div lassName={styles.navlink}>
                 <Link href={`/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F`}>
                     {curuser ?
-                    <h4 className={styles.profile}>Sign out</h4>:signIn                       
+                    <h4 className={styles.profile}>Sign In</h4>:signIn                       
                     }
                 </Link>
             </div>
