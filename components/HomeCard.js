611cb6d8a3ed3b8241c4b880af8f1116bf793760
root <root@Raheb.localdomain>
2022-12-08 18:50:41 +0100

    css add

--- components/HomeCard.js
+++ components/HomeCard.js
@@ -1,24 +1,22 @@
 import Link from "next/link"
+import Image from "next/image"
 import styles from "../styles/Card.module.css";
 
 export default function Component({ cake }) {
-  console.log('image url is======== ', cake.imageurl)
+  console.log('image url is ', cake.imageurl)
   return (
-    <>
+    <Link href={`/cakes/${cake.id}`}>
       <div className={styles.card} >
         <div className={styles.cardimg}>
-          <img src={cake.imageUrl} className={styles.cardimg1}/>
+          <Image alt="cake image" height={1000} width={700} src={cake.imageUrl} className={styles.cardimg1}/>
         </div>
         <div className={styles.cardDetails}>
           <h5>{cake.name}</h5>
           <p>{cake.description}</p>
           <p>{cake.location}</p>
           <p>{cake.price}</p>
-          <a href={`/cakes/${cake.id}`} className={styles.selectOne} >
-          <div className={styles.button}>Select</div>
-          </a>  
         </div>
       </div>
-    </>
+    </Link>
   )
 }
--- pages/cakes/[id].js
+++ pages/cakes/[id].js
@@ -1,66 +1,58 @@
 import styles from '../../styles/Select.module.css'
 import Navbar from "../../components/navbar1"
-import  cakeController from '../../controllers/cakeController'
+import cakeController from '../../controllers/cakeController'
 import { Input } from 'reactstrap'
 
-export default function oneCake(props){
+export default function oneCake(props) {
   const cakes = props.cake
-  console.log("cakes: " , cakes)
-    return (
-      <>
+  console.log("cakes: ", cakes)
+  return (
+    <>
       <Navbar></Navbar>
-      <h3 className= {styles.wording}>You are just one step away.....</h3>
-      <div class={styles.container}>
-        <div class={styles.row}>
-          <div class={styles.column}>
-            
-            <div class={styles.image1}>
-              <div className={styles.box1}>Image 1</div>
-            </div>
-          </div>
+      <h3 className={styles.wording}>You are just one step away.....</h3>
+      <div class={styles.slidShow}>
+        <div className={styles.rowImg}>
+        <div className={styles.columnImg1}>
+          <img src={cakes.imageUrl} className={styles.cardimg1} />
+          <img src={cakes.imageUrl} className={styles.cardimg2} />
+          <img src={cakes.imageUrl} className={styles.cardimg3} />
+        </div>  
+        <div className={styles.columnImg2}>
+          <img src={cakes.imageUrl} className={styles.cardimg4} />
+        </div>
+        </div>
+      </div>
 
-          <div class={styles.column}>
-            <div class={styles.sub1}>
-              <div className={styles.box2}>sub-Image1</div>
-            </div>
-            <div class={styles.sub2}>
-              <div className={styles.box2}>sub-Image2</div>
-            </div>
-            <div class={styles.sub3}>
-              <div className={styles.box3}>sub-Image3</div>
-          </div>
-          </div>
-          <div class={styles.column}>
-            
-            <div class={styles.info}>
-              <div className={styles.box4}>
-                <h1>{cakes.name}</h1>
-                <h4>{cakes.description}</h4>
-                <h4>Price: ${cakes.price}</h4>
-                <h4>{cakes.imageUrl}</h4>
-                
-                <h2>Bakery Information</h2>
-                <h4>Address: {cakes.location}</h4>
-                <input type="submit" className={styles.button} value="Order" />
-              </div>
+
+
+        <div class={styles.column}>
+
+          <div class={styles.info}>
+            <div className={styles.box4}>
+              <h1>{cakes.name}</h1>
+              <h4>{cakes.description}</h4>
+              <h4>Price: ${cakes.price}</h4>
+              <h4>{cakes.imageUrl}</h4>
+
+              <h2>Bakery Information</h2>
+              <h4>Address: {cakes.location}</h4>
+              <input type="submit" className={styles.button} value="Order" />
             </div>
           </div>
         </div>
-     </div>
-              
-      </>
-               
+
+    </>
+
   )
-            
+
 }
-          
-      
-         
+
+
+
 export async function getServerSideProps(req, res) {
-    const { id } = req.query
-    const cake = await cakeController.find(id)
-    return {
-      props: { cake },
-    }
+  const { id } = req.query
+  const cake = await cakeController.find(id)
+  return {
+    props: { cake },
   }
-  
+}
