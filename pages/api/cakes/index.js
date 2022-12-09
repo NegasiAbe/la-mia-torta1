fbd16769ba20fc1b08ee246e3d313906a21ac5f9
Asadullah Hassani <asadullah.hassani293@gmail.com>
2022-12-09 17:59:18 +0100

    edited

--- pages/api/cakes/index.js
+++ pages/api/cakes/index.js
@@ -1,16 +1,14 @@
 import cakeController from '../../../controllers/cakeController'
+import { getSession } from 'next-auth/react';
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
         // TODO take the title and description from the request body
-        const { name, description, price, location, imageUrl } = req.body
+  
         console.log('the request from the form', req.body)
         const UserId = 1
-        const cake = await cakeController.create({
-            name, description, price, location, imageUrl, UserId
-        })
+        const cake = await cakeController.create(UserId, req.body)
         res.status(200).redirect(`/baker`);
     }
     // the redirect metod sends the user to the specified path
-
 }
