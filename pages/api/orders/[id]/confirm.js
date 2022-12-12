63c658d6ac6fedea170f93c86f5f7837a164e453
yeshey choden <yeshey@DESKTOP-ES2KOVM.localdomain>
2022-12-12 12:45:01 +0100

    deleted customers

--- pages/api/orders/[id]/confirm.js
+++ pages/api/orders/[id]/confirm.js
@@ -1,4 +1,15 @@
 import ordersController from "../../../../controllers/orderController";
 
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        // TODO take the title and description from the request body
 
-const {id} = ordersController.confirm(req.query.id)
+        const session = await unstable_getServerSession(req, res, authOptions)
+        const UserId = (await db.User.findOne({ where: { email: session.user.email } })).id
+        
+        const {id} = await ordersController.confirm(req.query.id)
+        const order = await ordersController.create({ UserId: UserId, status: "pending", ...req.body }) //req.body gives the cakeid
+        res.status(200).redirect(`/orders/${order.id}`)
+    }
+    
+}
