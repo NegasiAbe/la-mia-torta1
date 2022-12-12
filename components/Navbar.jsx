9347fdd9d8594bef669aba67223a358bec8e11eb
yeshey choden <yeshey@DESKTOP-ES2KOVM.localdomain>
2022-12-12 17:39:30 +0100

    a ti link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Component() {
             <div className={styles.container}>
                 <nav className={styles.nav}>
                     <div >
-                    <a href="/" ><Image src="https://res.cloudinary.com/dlmrmq1tl/image/upload/v1670770166/LaMiaTorta/logo-withoutBG_sbnqri.png" height="200" width="200" alt="logo" className={styles.navBranding}/></a>
+                    <Link href="/" ><Image src="https://res.cloudinary.com/dlmrmq1tl/image/upload/v1670770166/LaMiaTorta/logo-withoutBG_sbnqri.png" height="200" width="200" alt="logo" className={styles.navBranding}/></Link>
                     </div>
                     <ul className={styles.navMenu}>
                         <li className={styles.navItem}>
@@ -21,11 +21,11 @@ export default function Component() {
                                     <i className="fa fa-caret-down">   ▼</i>
                                 </button>
                                 <div className={styles.dropdownContent}>
-                                <a href="/bakers">Profile</a>
-                                <a href="/bakers/new">Add New Cake</a>
-                                <a href="/bakers/orders">Orders</a>
-                                <a href="/profile">Change Passwrod</a>
-                                <a href="/" onClick={() => signOut()} value="action">Log out</a>
+                                <Link href="/bakers">Profile</Link>
+                                <Link href="/bakers/new">Add New Cake</Link>
+                                <Link href="/bakers/orders">Orders</Link>
+                                <Link href="/profile">Change Passwrod</Link>
+                                <Link href="/" onClick={() => signOut()} value="action">Log out</Link>
                                 </div>
                             </div>
                         </li>
